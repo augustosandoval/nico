1567f3ca1d30ecfed51cc8819f75c7235b9e5c9b
asandoval@guidewire.com <asandoval@guidewire.com>
2021-07-22 16:01:25 -0400

    Better code view

--- php/list.php
+++ php/list.php
@@ -4,19 +4,18 @@
 <title>User List</title>
 </head>
 <body>
-<h1 align=center>User Records</h1>
-<table align="center" border="1px" style="width:600px; line-height:40px;"> 
-<tr>
-<th colspan="6"><h2>User Records</h2></th>
-</tr>
-<tr>
-<th>Name</th>
-<th>Age</th>
-<th>Sex</th>
-<th>Us Citizen</th>
-<th>ID</th>
-<th>Select</th>
-</tr>
+    <h1 align=center>User Records</h1>
+    <table align="center" border="1px" style="width:600px; line-height:40px;"> 
+    <tr>
+    <th colspan="6"><h2>User Records</h2></th>
+    </tr>
+    <th>Name</th>
+    <th>Age</th>
+    <th>Sex</th>
+    <th>Us Citizen</th>
+    <th>ID</th>
+    <th>Select</th>
+    </tr>
 
 <?php
 $servername="localhost";
@@ -27,19 +26,28 @@ $dbname="database1";
 $conn=mysqli_connect($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-die ("Connection Failed: " . $conn->connect_error);
+    die ("Connection Failed: " . $conn->connect_error);
 } 
 
-$sql="SELECT Name, Age, Sex, US_Citizen, ID FROM users";
+$sql = "SELECT Name, Age, Sex, US_Citizen, ID FROM users";
 $result=$conn->query($sql);
 
 if ($result->num_rows>0) {
-while($row=$result->fetch_assoc()) {
-echo "<tr><td>" . $row["Name"] . "</td><td>" . $row["Age"] . "</td><td>" . $row["Sex"] . "</td><td>" . $row["US_Citizen"] . "</td><td>" . $row["ID"] . "</td></tr>";
-}
+    while($row=$result->fetch_assoc()) {
+        $line = "<tr>"
+        $line .= "<td>" . $row["Age"] . "</td>"
+        
+
+        echo $line
+
+
+        //echo "<tr><td>" . $row["Name"] . "</td><td>" . $row["Age"] . "</td><td>" . $row["Sex"] . "</td><td>" . $row["US_Citizen"] . "</td><td>" . $row["ID"] . "</td></tr>";
+    }
 echo "</table>";
-}else {
-echo "0 results";
+
+} else {
+    echo "0 results";
+
 }
 $conn->close();
 ?>
