cd542e809441bcb7cd98761a0059feabb3da46cb
NicolasSchmidt004 <87498040+NicolasSchmidt004@users.noreply.github.com>
2021-07-22 15:55:28 -0400

    Add files via upload

--- php/list.php
+++ php/list.php
@@ -7,13 +7,15 @@
 <h1 align=center>User Records</h1>
 <table align="center" border="1px" style="width:600px; line-height:40px;"> 
 <tr>
-<th colspan="4"><h2>User Records</h2></th>
+<th colspan="6"><h2>User Records</h2></th>
 </tr>
 <tr>
 <th>Name</th>
 <th>Age</th>
 <th>Sex</th>
 <th>Us Citizen</th>
+<th>ID</th>
+<th>Select</th>
 </tr>
 
 <?php
@@ -28,12 +30,12 @@ if ($conn->connect_error) {
 die ("Connection Failed: " . $conn->connect_error);
 } 
 
-$sql="SELECT Name, Age, Sex, US_Citizen FROM User";
+$sql="SELECT Name, Age, Sex, US_Citizen, ID FROM users";
 $result=$conn->query($sql);
 
 if ($result->num_rows>0) {
 while($row=$result->fetch_assoc()) {
-echo "<tr><td>" . $row["Name"] . "</td><td>" . $row["Age"] . "</td><td>" . $row["Sex"] . "</td><td>" . $row["US_Citizen"] . "</td></tr>";
+echo "<tr><td>" . $row["Name"] . "</td><td>" . $row["Age"] . "</td><td>" . $row["Sex"] . "</td><td>" . $row["US_Citizen"] . "</td><td>" . $row["ID"] . "</td></tr>";
 }
 echo "</table>";
 }else {
